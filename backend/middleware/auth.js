286af85cdeebd305b5d4a101268ae60f20807ad7
Ajay kumar <kajay77598@gmail.com>
2026-04-19 17:27:18 +0530

    fied

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,17 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  const token = req.header('Authorization');
+  const authHeader = req.header("Authorization");
 
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, access denied' });
+  if (!authHeader) {
+    return res.status(401).json({ msg: "No token ❌" });
   }
 
   try {
-    const decoded = jwt.verify(token, 'secretkey');
+    const token = authHeader.split(" ")[1]; // Bearer TOKEN
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ msg: 'Invalid token' });
+    res.status(401).json({ msg: "Invalid token ❌" });
   }
 };
